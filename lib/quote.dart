9289c09af69304a93955f5de2c9f1dc7d7e8824c
Safwan Saafi <saafi.saf0@gmail.com>
2025-02-07 09:16:42 +0100

    Structure of the app added ( pages )

--- lib/quote.dart
+++
@@ -1,9 +0,0 @@
-class Quote {
-  
-  String text;
-  String author;
-  // constructor with named parameters
-  // & automatically assigns named arguments to class properties
-  Quote({ required this.text, required this.author });
-
-}
