eca12b245d603d2e1cad7e0044678a8a91b22952
Safwan Saafi <saafi.saf0@gmail.com>
2025-02-07 09:52:10 +0100

    Asynchronous code example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_time_api/pages/home.dart';
+import 'package:flutter_application_time_api/pages/loading.dart';
+import 'package:flutter_application_time_api/pages/choose_location.dart';
+
 void main() => runApp(MaterialApp(
-  home: Home(),
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }
 ));
